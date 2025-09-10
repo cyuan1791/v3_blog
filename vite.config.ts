a3b3cbbc2b49284e61fdd0ab4b327a0390b13431
Mark Yuan <mark@webasone.com>
2025-09-10 18:11:31 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '_PagePath_',
+  base: "_PagePath_",
   plugins: [vue()],
-})
+});
